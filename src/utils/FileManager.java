407241b0e943e4417822941653a7d29a0e0524cd
matthew_go <matthew.go123@gmail.com>
2017-03-27 11:31:37 +0800

    Parsed News and Opinion Corpus from Ralph

--- src/utils/FileManager.java
+++ src/utils/FileManager.java
@@ -7,6 +7,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -45,6 +46,23 @@ public class FileManager {
 		bufferWriter.close();
 	}
 
+	public static List<String> readFileISO88591(File f) throws IOException {
+		List<String> lines = new ArrayList<>();
+		InputStream fr = new FileInputStream(f);
+		byte[] buffer = new byte[4096];
+		while (true) {
+			int byteCount = fr.read(buffer, 0, buffer.length);
+			if (byteCount <= 0) {
+				break;
+			}
+
+			String s = new String(buffer, 0, byteCount, "ISO-8859-1");
+			System.out.println(s);
+			lines.add(s);
+		}
+		return lines;
+	}
+
 	public static List<String> readFile(File f) throws FileNotFoundException, IOException {
 		List<String> lines = new ArrayList<>();
 
@@ -56,5 +74,17 @@ public class FileManager {
 		}
 
 		return lines;
+	}
+
+	public static List<File> getFiles(File rootFolder, String filetype) {
+		List<File> files = new ArrayList<>();
+		for (File file : rootFolder.listFiles()) {
+			if (file.getPath().substring(file.getPath().length() - 3).toLowerCase().equals(filetype))
+				files.add(file);
+			else if (file.isDirectory()) {
+				files.addAll(getFiles(file, filetype));
+			}
+		}
+		return files;
 	}
 }
